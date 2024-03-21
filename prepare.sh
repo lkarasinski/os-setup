b36f4a52ea44c6368ff2296ba5d8efa46746eb69
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2024-03-21 14:03:17 +0000

    fix: add noconfirm to yay installation

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Syu --noconfirm
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
