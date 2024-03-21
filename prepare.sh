bd7553bd71373bb4770e6ded7a63a58e2ebb866b
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2024-03-21 13:58:11 +0000

    feat: add pacman-key initialization to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
+pacman-key --init
+pacman-key --populate archlinux
+pacman -Syu --noconfirm
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
