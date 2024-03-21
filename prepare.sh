f66900fc5e28ef666b410fd949f5a49652dd865d
Łukasz Karasiński <lukasz.karasinski.15@gmail.com>
2024-03-21 14:06:53 +0000

    feat: add reflector to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,8 @@
 pacman-key --init
 pacman-key --populate archlinux
 pacman -Syu --noconfirm
+pacman -S --noconfirm reflector
+reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
