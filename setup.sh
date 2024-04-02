4f7ae439b65cc11aaf52b2b11d030fc835b04102
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2024-04-02 13:32:30 +0200

    refactor: install yay with ansible role

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ pacman -Syu --noconfirm
 pacman -S --noconfirm reflector
 # Temporarily skip reflector, because it takes too long to run. Will enable when the config is ready.
 # reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 pacman -S --noconfirm ansible-core git
 ansible-galaxy collection install community.general
+ansible-galaxy role install jahrik.yay
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install kewlfft.aur
 git clone https://github.com/lkarasinski/arch-playbook /tmp/arch-playbook
