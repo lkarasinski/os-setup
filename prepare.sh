053d4b813f8fde0d977efb91740eef0b4d86740a
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2024-04-02 11:07:24 +0200

    feat: update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,9 @@ pacman-key --populate archlinux
 pacman -Sy --needed --noconfirm archlinux-keyring && pacman --noconfirm -Su
 pacman -Syu --noconfirm
 pacman -S --noconfirm reflector
-reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+# Temporarily skip reflector, because it takes too long to run. Will enable when the config is ready.
+# reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+pacman -S --noconfirm ansible-core
 ansible-galaxy collection install community.general
 ansible-galaxy collection install ansible.posix
