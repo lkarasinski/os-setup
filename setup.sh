aadb59246679a4098dee367dccd9f59eb7833804
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2024-04-13 11:02:23 +0200

    refactor: rename repository

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 BRANCH=${1:-main}
-curl -Ls "https://raw.githubusercontent.com/lkarasinski/arch-playbook/$BRANCH/prepare-arch.sh" | sh
+curl -Ls "https://raw.githubusercontent.com/lkarasinski/os-setup/$BRANCH/prepare-arch.sh" | sh
 
-# Make sure there is no /tmp/arch-playbook directory
-sudo rm -rf /tmp/arch-playbook
+# Make sure there is no /tmp/os-setup directory
+sudo rm -rf /tmp/os-setup
 
-git clone -b "$BRANCH" https://github.com/lkarasinski/arch-playbook /tmp/arch-playbook
-ansible-playbook /tmp/arch-playbook/setup-user.yml
+git clone -b "$BRANCH" https://github.com/lkarasinski/os-setup /tmp/os-setup
+ansible-playbook /tmp/os-setup/setup-user.yml
 
-sudo -u lkarasinski -H sh -c "ansible-galaxy install -r /tmp/arch-playbook/requirements.yml"
-sudo -u lkarasinski -H sh -c "ansible-playbook /tmp/arch-playbook/arch-playbook.yml --ask-vault-pass"
+sudo -u lkarasinski -H sh -c "ansible-galaxy install -r /tmp/os-setup/requirements.yml"
+sudo -u lkarasinski -H sh -c "ansible-playbook /tmp/os-setup/os-setup.yml --ask-vault-pass"
