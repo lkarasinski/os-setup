d3d57c57000c5d198a86634a4280b8b6451a73af
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2024-04-13 10:32:38 +0200

    refactor: move dependencies to requirements.yml

--- setup.sh
+++ setup.sh
@@ -7,10 +7,5 @@ sudo rm -rf /tmp/arch-playbook
 git clone -b "$BRANCH" https://github.com/lkarasinski/arch-playbook /tmp/arch-playbook
 ansible-playbook /tmp/arch-playbook/setup-user.yml
 
-sudo -u lkarasinski -H sh -c "ansible-galaxy collection install community.general"
-sudo -u lkarasinski -H sh -c "ansible-galaxy role install jahrik.yay"
-sudo -u lkarasinski -H sh -c "ansible-galaxy collection install ansible.posix"
-sudo -u lkarasinski -H sh -c "ansible-galaxy collection install kewlfft.aur"
-
+sudo -u lkarasinski -H sh -c "ansible-galaxy install -r /tmp/arch-playbook/requirements.yml"
 sudo -u lkarasinski -H sh -c "ansible-playbook /tmp/arch-playbook/arch-playbook.yml --ask-vault-pass"
-
